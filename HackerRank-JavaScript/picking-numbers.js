7e807cb96f83aef42201f5bf42446173cf749e31
ChristianOtieno <cgao273@gmail.com>
2019-09-19 22:55:05 +0300

    Picking Numbers

---
+++ HackerRank-JavaScript/picking-numbers.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    let pickingarray = a.sort((a, b) => a - b);
+
+    if (a.length <= 1) {
+        return 0;
+    }
+    let maxCount = 0;
+    let count = 0;
+    for (let i = 1; i < pickingarray.length; i++) {
+        if (Math.abs(pickingarray[i] - pickingarray[i + 1]) <= 1) {
+            maxCount++;
+        }
+    }
+    return maxCount;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
