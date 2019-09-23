e20e3bfa2aced1256d077e2d20c5dc6a520ff9bf
ChristianOtieno <cgao273@gmail.com>
2019-09-23 17:33:54 +0300

    The hurdle race solution

---
+++ the-hurdle-race.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the hurdleRace function below.
+function hurdleRace(k, height) {
+
+    let ans = 0;
+    let max = height[0]
+    for (let i = 1; i < height.length - 1; i++) {
+        if (height[i] > max)
+            max = height[i];
+    }
+    if (max >= k) {
+        ans = max - k;
+    }
+    return ans;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const height = readLine().split(' ').map(heightTemp => parseInt(heightTemp, 10));
+
+    let result = hurdleRace(k, height);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
